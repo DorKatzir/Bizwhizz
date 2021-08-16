e5cf2bb7b781acdecb0c04ccda8200ffccaab3f1
DorKatzir <DorKatzir@users.noreply.github.com>
2021-08-16 23:37:06 +0300

    javascript completed carusel slider

--- js/caruselNav.js
+++ js/caruselNav.js
@@ -1,1 +1,32 @@
-console.log("hello Dor")
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+let cardIndex; // Holds the current slide number
+let slideNum; // Holds the next slide number
+
+
+rightArrow.addEventListener('click', function(){
+    cardIndex = document.querySelector('input[name="slide"]:checked').value;
+    if(cardIndex < 3){
+        cardIndex++;
+    }
+    else{
+        cardIndex = 1;
+    }
+    slideNum = 'slide' + cardIndex;
+    document.getElementById(slideNum).checked = true;
+    
+});
+
+leftArrow.addEventListener('click', function(){
+    cardIndex = document.querySelector('input[name="slide"]:checked').value;
+    if(cardIndex > 1){
+        cardIndex--;
+    }
+    else{
+        cardIndex = 3;
+    }
+    slideNum = 'slide' + cardIndex;
+    document.getElementById(slideNum).checked = true;
+    
+});
